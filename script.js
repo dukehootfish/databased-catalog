fd7fc6aec636992faf7f201db9fd20909f2af668
dukehootfish <77045140+dukehootfish@users.noreply.github.com>
2023-08-15 21:35:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const API_KEY = 'patFPhQ2NCGDqLUJR.14c2310f47d18237673940cc7428eeec8d471ffbb4510d81d099aeec97c48fae';
+const BASE_ID = 'appHQzwgShaDAx9Aw';
+
+const bookList = document.getElementById('bookList');
+
+async function fetchBooks() {
+    try {
+        const response = await fetch(`https://api.airtable.com/v0/${BASE_ID}/Books`, {
+            headers: {
+                Authorization: `Bearer ${API_KEY}`,
+            },
+        });
+        const data = await response.json();
+
+        data.records.forEach((record) => {
+            const book = record.fields;
+            const li = document.createElement('li');
+            li.innerHTML = `<strong>${book.Title}</strong> by ${book.Author} (${book.Genre})`;
+            bookList.appendChild(li);
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+fetchBooks();// JavaScript source code

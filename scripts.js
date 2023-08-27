b4f14d3b1f9b56b7e98875beb731c5cc65806783
dukehootfish <77045140+dukehootfish@users.noreply.github.com>
2023-08-27 04:09:38 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,29 +1,4 @@
-/*const PERSONAL_ACCESS_TOKEN = 'patFPhQ2NCGDqLUJR.14c2310f47d18237673940cc7428eeec8d471ffbb4510d81d099aeec97c48fae';
-const BASE_ID = 'appHQzwgShaDAx9Aw';
 
-const bookList = document.getElementById('bookList');
-
-async function fetchBooks() {
-    try {
-        const response = await fetch(`https://api.airtable.com/v0/${BASE_ID}/Books`, {
-            headers: {
-                Authorization: `Bearer ${PERSONAL_ACCESS_TOKEN}`,
-            },
-        });
-        const data = await response.json();
-
-        data.records.forEach((record) => {
-            const book = record.fields;
-            const li = document.createElement('li');
-            li.innerHTML = `<strong>${book.Title}</strong> by ${book.Author} (${book.Genre})`;
-            bookList.appendChild(li);
-        });
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-}
-
-fetchBooks();// JavaScript source code */
 const PERSONAL_ACCESS_TOKEN = 'patFPhQ2NCGDqLUJR.14c2310f47d18237673940cc7428eeec8d471ffbb4510d81d099aeec97c48fae';
 const airtableBaseId = 'appHQzwgShaDAx9Aw';
 
@@ -43,30 +18,48 @@ async function fetchProducts() {
   }
 }
 
+// ... (previous code)
+
 async function displayProducts() {
   const products = await fetchProducts();
 
   const productContainer = document.getElementById('product-list');
 
   products.forEach(product => {
-    const { Name, Description, ImageUrl } = product.fields;
+    const { Name, ImageUrl, Link } = product.fields;
 
     const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
+    productCard.classList.add('col-md-6', 'col-lg-4');
+    productCard.id = 'cat-box';
 
-    const name = document.createElement('h2');
-    name.textContent = Name;
+    const cardWrapper = document.createElement('div');
+    cardWrapper.classList.add('card', 'border-0');
+    cardWrapper.style.background = 'rgba(255, 255, 255, 0)';
 
-    const image = document.createElement('img');
-    image.src = ImageUrl;
-    image.alt = Name;
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body', 'cat-card-body');
 
-    const description = document.createElement('p');
-    description.textContent = Description;
-      
-    productCard.appendChild(name);
-    productCard.appendChild(image);
-    productCard.appendChild(description);
+    const productNameLink = document.createElement('a');
+    productNameLink.id = 'cat-name';
+    productNameLink.innerHTML = `<strong>${Name}</strong>`;
+    cardBody.appendChild(productNameLink);
+
+    cardWrapper.appendChild(cardBody);
+
+    const productImage = document.createElement('img');
+    productImage.classList.add('imageicon');
+    productImage.src = ImageUrl;
+
+    const linkButton = document.createElement('a');
+    linkButton.classList.add('btn', 'btn-primary', 'btn-sm');
+    linkButton.role = 'button';
+    linkButton.id = 'link_button';
+    linkButton.href = Link;
+    linkButton.textContent = 'לרשימת הספקים >';
+
+    productCard.appendChild(cardWrapper);
+    productCard.appendChild(productImage);
+    productCard.appendChild(linkButton);
 
     productContainer.appendChild(productCard);
   });
